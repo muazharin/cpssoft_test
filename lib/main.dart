9bf22ca44588809e5a5621a45350c9deac8dcd3e
Muazharin Alfan <alfanmuazharin@gmail.com>
2022-10-24 14:34:28 +0700

    refactor(adding multiprovider at main)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:cpssoft_test/bloc/city_bloc.dart';
 import 'package:cpssoft_test/bloc/user_bloc.dart';
 import 'package:cpssoft_test/routes.dart';
 import 'package:cpssoft_test/services/locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -11,17 +13,23 @@ void main() {
 
 class MyApp extends StatelessWidget {
   final UserBloc userBloc = UserBloc();
+  final CityBloc cityBloc = CityBloc();
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'CPSSoft',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => userBloc),
+        BlocProvider(create: (_) => cityBloc),
+      ],
+      child: GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'CPSSoft',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/user',
+        getPages: getPages,
       ),
-      initialRoute: '/user',
-      defaultTransition: Transition.fade,
-      getPages: getPages,
     );
   }
 }
